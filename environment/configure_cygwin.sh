7fd93e6094b83db1d12248626a433d58f9a3cb12
Ian R. O'Brien <ian.r.obrien@gmail.com>
2016-11-17 10:13:38 -0500

    Update configure_cygwin.sh

--- environment/configure_cygwin.sh
+++ environment/configure_cygwin.sh
@@ -1,1 +1,12 @@
+# This script will set up a cygwin environment (using zsh)
+# 
+# Installs:
+#   Applications:
+#     1. oh-my-zsh
+#   Configurations:
+#
+# Dependencies:
+#   1. zsh
+#   2. git
+
 #!/usr/bin/env bash
