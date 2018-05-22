5149c5072677ff472c566b237d0aacbc6f65be02
Ian R. O'Brien <ian.r.obrien@protonmail.com>
2018-05-22 22:35:40 +0200

    adding subdirectories - probably will need to fix this

--- applications/bash/install.sh
+++ applications/bash/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-ln -sfr ./.bash_aliases ~
-ln -sfr ./.git-completion.bash ~
-ln -sfr ./.git-prompt.sh ~
-ln -sfr ./.ianrobrien_bash_prompt ~
+ln -sfr ./bash/.bash_aliases ~
+ln -sfr ./bash/.git-completion.bash ~
+ln -sfr ./bash/.git-prompt.sh ~
+ln -sfr ./bash/.ianrobrien_bash_prompt ~
 
 source ~/.bashrc
