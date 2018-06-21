5e9152f082bd69a034dad242f92fbbcb006e42b0
Ian R. O'Brien <ian.r.obrien@protonmail.com>
2018-06-21 14:58:25 +0200

    added more formatting

--- configure-current-user.sh
+++ configure-current-user.sh
@@ -18,16 +18,16 @@ fi
 extras_dir=${PWD}/extras/
 wallpaper_source_dir=${extras_dir}wallpaper/
 wallpaper_target_dir=${HOME}/Pictures/Wallpapers/
-gnome_customization_scripts=${PWD}/user-customization-scripts/
+customization_scripts_dir=${PWD}/user-customization-scripts/
 wallpaper_name="trolltunga-1920x1200.jpg"
 
 echo "********************************************************************************"
 echo "Customizing gnome..."
 
 # Appearance
-${gnome_customization_scripts}install-McOS-MJV-Dark-mode.sh
-${gnome_customization_scripts}install-capitaine-cursors.sh
-${gnome_customization_scripts}install-fonts.sh
+${customization_scripts_dir}install-McOS-MJV-Dark-mode.sh
+${customization_scripts_dir}install-capitaine-cursors.sh
+${customization_scripts_dir}install-fonts.sh
 
 ## Background and Lockscreen Image
 mkdir -p ${wallpaper_target_dir}
@@ -36,13 +36,8 @@ gsettings set "org.gnome.desktop.background" "picture-uri" "file://${wallpaper_t
 gsettings set "org.gnome.desktop.screensaver" "picture-uri" "file://${wallpaper_target_dir}${wallpaper_name}"
 
 # Application Settings
-${gnome_customization_scripts}load-gnome-terminal-settings.sh
+${customization_scripts_dir}load-gnome-terminal-settings.sh
 gsettings set "org.gnome.shell.extensions.dash-to-dock" "click-action" "minimize"
-
-# To Do:
-## Splash Screen
-## Login Screen
-## Grub Theme
 
 ${PWD}/dotfiles/install-user-dot-files.sh
 
--- configure-system.sh
+++ configure-system.sh
@@ -12,15 +12,16 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# Default Applications
+# Applications
 apt update && apt install -y \
     vim \
     filezilla thunderbird libreoffice \
     htop gdebi \
     git docker.io openjdk-11-jre openjdk-11-jdk openjdk-8-jdk openjdk-8-jre \
-    gnome-tweak-tool \
+    gnome-tweak-tool gnome-system-monitor gnome-calculator \
     fortunes cowsay lolcat papirus-icon-theme fonts-firacode
 
+# Application Settings
 gsettings set "org.gnome.desktop.interface" "icon-theme" "Papirus"
 gsettings set "org.gnome.desktop.interface" "monospace-font-name" "Fira Code 12"
 
@@ -31,3 +32,8 @@ fi
 if ! [[ -x `(command -v bcompare)` ]]; then
     ${PWD}/application-installation-scripts/install-beyond-compare.sh
 fi
+
+# To Do (Probably in system settings)
+## Splash Screen
+## Login Screen
+## Grub Theme
